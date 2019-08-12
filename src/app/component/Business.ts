88529ca1e38db6cbf6e77a434a94f451d5dca040
Khuzaima Shahid <khuzaima.shahid1@gmail.com>
2019-08-12 14:34:32 +0100

    Final Commit

---
+++ src/app/component/Business.ts
@@ -0,0 +1,19 @@
+export default class Business {
+    name: String;
+    number: String;
+    description: String;
+    make: String;
+    model: String;
+    version:String;
+    transmission: String;
+    color: String;
+    mileage: String;
+    yom: String;
+    refrence: String;
+    note: String;
+    availability: String;
+    price: String;
+    image: String;
+    status: String;
+    create_date: String;
+  }
